122bd4822d28c7f80422c50c2af829e22d64c0c4
leenadiharicmyk <leenadihari@gmail.com>
2026-02-14 15:12:38 +0530

    database changes

--- database/db.py
+++ database/db.py
@@ -2,5 +2,9 @@ import sqlite3
 from config import DB_PATH
 
 def get_db_connection():
-    return sqlite3.connect(DB_PATH)
+    conn = sqlite3.connect(DB_PATH)
 
+  
+    conn.row_factory = sqlite3.Row
+
+    return conn
